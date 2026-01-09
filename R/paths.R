234420acbbc4583adc299466c37a366a583984f2
clkuryla <clkuryla@gmail.com>
2026-01-09 13:28:14 -0500

    Add comments in R/paths.R to explain the purpose and how to use it

--- R/paths.R
+++ R/paths.R
@@ -1,3 +1,9 @@
+# Paths helper
+
+# This function enables us to store large files in a separate data depot
+# This function assumes an environment variable DATA_DEPOT and SRH_DATA that points to the root of the data depot (see comments below the code)
+# Alternatively, you can just set the data_root() function to a hard-coded path on your system
+
 library(here)
 
 data_root <- function() {
@@ -13,3 +19,32 @@ ensure_dirs <- function() {
   dir.create(derived_path(), recursive = TRUE, showWarnings = FALSE)
   dir.create(here::here("outputs"), recursive = TRUE, showWarnings = FALSE)
 }
+
+###### How to use this function/process #####
+#
+# Set up the data depot (once)
+# Outside the repo, create:
+#   
+#   ~/data_depot/
+#     surveys/
+#       MEPS/
+#       NHIS/
+#       BRFSS/
+#       CPS/
+#.      GSS
+#       NHANES
+#     _derived/
+#       srh_ project/
+#   
+#   
+#  Then set the DATA_DEPOT environment variable:
+#      install.packages("usethis")
+#.     usethis::edit_r_environ()
+#   Then add this to ~/.Renviron:
+#      SRH_DATA=/Users/yourusername/data_depot/surveys
+#      DATA_DEPOT=/Users/yourusername/data_depot
+#.  Restart RStudio
+#   Test that it works:
+#      Sys.getenv("DATA_DEPOT")
+#      dir.exists(Sys.getenv("DATA_DEPOT"))
+#
